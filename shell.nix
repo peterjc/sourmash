eadddb0335e4f4bfe7f9a1342e8083b41107e342
Luiz Irber <luiz.irber@gmail.com>
2021-03-16 15:53:37 -0700

    fix shell.nix with new deps

--- shell.nix
+++ shell.nix
@@ -10,6 +10,9 @@ in
       rustPlatform.rust.cargo
       git
       stdenv.cc.cc.lib
+      llvmPackages.libclang
+      cmake
+      rust-bindgen
       (python38.withPackages(ps: with ps; [ virtualenv tox setuptools ]))
       (python39.withPackages(ps: with ps; [ virtualenv setuptools ]))
       (python37.withPackages(ps: with ps; [ virtualenv setuptools ]))
@@ -21,5 +24,6 @@ in
        # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
        export SOURCE_DATE_EPOCH=315532800 # 1980
        export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+       export LIBCLANG_PATH="${llvmPackages.libclang}/lib";
     '';
   }
